4791756fd6c175b004b8853222f21117dfe23634
EvelynDevia29 <evelynquevedo817@gmail.com>
2022-02-04 16:49:16 -0500

    ECMAScript7+

---
+++ src/es7/index.js
@@ -0,0 +1,15 @@
+/*//VALIDAR SI EN EL ARREGLO SE INCLUYE EL NUMERO 7*/
+
+let numbers = [1,2,3,7,8];
+
+if(numbers.includes(9)){
+    console.log("Si se encuentra el valor 7");
+}else{
+    console.log("No se encuentra el valor 7");
+}
+
+/**EXPONENTES USANDO ** */
+let base = 4;
+let exponent = 4;
+let result = base**exponent;
+console.log(result);
