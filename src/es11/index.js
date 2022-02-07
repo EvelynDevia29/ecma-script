fa5cd45517a72be15cadaf2e8678591038da840e
EvelynDevia29 <evelynquevedo817@gmail.com>
2022-02-07 16:28:19 -0500

    ES11+ dynamc¡ic import

---
+++ src/es11/index.js
@@ -0,0 +1,9 @@
+/**dinamic import, importacion de elementos de forma 
+ * dinamica */
+
+ const button = document.getElementById("btn");
+
+ button.addEventListener("click", async function(){
+     const module = await import("./file.js");
+     module.hello();
+ });
