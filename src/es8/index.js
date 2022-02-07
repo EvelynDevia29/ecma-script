2bceb4fd86fa0fd6bd08193e3c44f59b07fd0653
EvelynDevia29 <evelynquevedo817@gmail.com>
2022-02-07 14:26:01 -0500

    fin ECMAScript8+, asyn, await

--- src/es8/index.js
+++ src/es8/index.js
@@ -1,9 +1,9 @@
 /**  Object.entries crea una matriz*/
 
 const data = {
-    frontend:'Monica',
-    backend:'Isabel',
-    design:'Juana'
+    frontend: 'Monica',
+    backend: 'Isabel',
+    design: 'Juana'
 };
 
 const entries = Object.entries(data);
@@ -13,9 +13,9 @@ console.log(entries.length); //saber cuantos elementos hay
 
 /**Trae los valores en un arreglo */
 const data = {
-    frontend:'Monica',
-    backend:'Isabel',
-    design:'Juana'
+    frontend: 'Monica',
+    backend: 'Isabel',
+    design: 'Juana'
 };
 
 const values = Object.values(data);
@@ -25,5 +25,32 @@ console.log(values.length);
 /**Padding */
 const string = 'hello';
 
-console.log(string.padStart(7,'hi'));
-console.log(string.padEnd(12,'-------'));
+console.log(string.padStart(7, 'hi'));
+console.log(string.padEnd(12, '-------'));
+
+/**IF TERNARIO, ASYGN AWAIT*/
+
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (false)
+            ? setTimeout(() => resolve('Hello World'), 3000)
+            : reject(new Error('Test Error'))
+    })
+};
+
+const helloAsign = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+};
+
+helloAsign();
+
+const anotherFunction = async () => {
+    try{
+        const hello = await helloWorld();
+        console.log(hello);
+    }catch(error){
+        comsole.log(error);
+    }
+};
+anotherFunction();
